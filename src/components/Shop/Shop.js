4b21f80436b6f4944d7ec19f560f46797ff6e72f
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2022-03-26 15:47:11 +0600

    item add to cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProduct(data))
-    }, [])
+    }, []);
+    const handleAddToCart = (product) => {
+        if (cart.length < 4) {
+            const newCart = [...cart, product]
+            setCart(newCart);
+        }
+    }
+   
     return (
         <div className='shop-container'>
+
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h1>this is a test</h1>
+                <h2>Select Apple watch</h2>
+                {
+                    cart.map(item => <Cart key={item.id} cart={item}></Cart>)
+                }
+                {/* <Cart cart={cart}></Cart> */}
+                <button className='clear-cart'>Choose one for me</button>
+                <button className='review-order'>Clear</button>
             </div>
         </div>
     );
